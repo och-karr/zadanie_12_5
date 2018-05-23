4728e93a62fa336bc338d26a55e115a7e6fb39de
karolinneczka <okaa93@gmail.com>
2018-05-23 20:11:15 +0200

    Finish working quote machine

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var tweetLink = "https://twitter.com/intent/tweet?text="; 
+//link do wysylania tweetow na tweetera (bez tresci)
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+//link pobierajacy cytaty
+
+function getQuote() {
+	$.getJSON(quoteUrl, createTweet);
+	$.getJSON(prefix + quoteUrl, createTweet);
+	$.ajaxSetup({ cache: false });
+}
+
+//skrot od:
+// $.ajax({
+//     dataType: "json", - te wartosci zostana przyjete domyslnie
+//     url: quoteUrl, //adres zapytania (nasz link do API)
+//     data: null, - te wartosci zostana przyjete domyslnie
+//     success: createTweet //funkcja, która zostanie wykonana przy pomyślnym wykonaniu zapytania
+// });
+
+function createTweet(input) {
+    var data = input[0];
+
+	var quoteText = $(data.content).text().trim();
+	//data.content - klucz, kod html paragrafu
+	//text - wyciagamy text z data.content
+	//trim - ucinamy niepotrzebne spacje
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) { //lenght = 0 --> false, !length = 0 --> true
+        quoteAuthor = "Unknown author";
+	}
+	
+	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+	if (tweetText.length > 140) {
+		getQuote();
+	} else {
+		var tweet = tweetLink + encodeURIComponent(tweetText);
+		$('.quote').text(quoteText); //tresc cytatu
+		$('.author').text("Author: " + quoteAuthor); //autor cytatu
+		$('.tweet').attr('href', tweet);
+	}
+
+}
+
+$(document).ready(function() {
+	getQuote();
+	$('.trigger').click(function() {
+		getQuote();
+	})
+});
