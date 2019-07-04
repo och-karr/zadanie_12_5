81b25f57998fe7c97a7b1e973954c9634e2e338b
karolinneczka <okaa93@gmail.com>
2019-07-04 12:05:28 +0200

    Add some fixes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var prefix = "https://cors-anywhere.herokuapp.com/";
-var tweetLink = "https://twitter.com/intent/tweet?text="; 
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/"; //aby ominąć CORS
+var tweetLink = "https://twitter.com/intent/tweet?text="; //https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/guides/web-intent.html
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"; //https://quotesondesign.com/api-v4-0/
 
 function getQuote() {
 	$.getJSON(prefix + quoteUrl, createTweet);
@@ -15,18 +15,18 @@ function getQuote() {
 // });
 
 function createTweet(input) {
-    var data = input[0];
+	var data = input[0];
 
-	var quoteConent = $(data.content).text().trim();
-	var quoteText = quoteConent.charAt(0).toUpperCase() + quoteConent.substr(1).toLowerCase();
+	var quoteContent = $(data.content).text().trim();
+	var quoteText = quoteContent.charAt(0).toUpperCase() + quoteContent.substr(1).toLowerCase();
 	//text - wyciagamy text z data.content
 	//trim - ucinamy niepotrzebne spacje
-    var quoteAuthor = data.title;
+	var quoteAuthor = data.title;
 
-    if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
+	if (!quoteAuthor.length) {
+		quoteAuthor = "Unknown author";
 	}
-	
+
 	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
 	if (tweetText.length > 140) {
@@ -40,9 +40,9 @@ function createTweet(input) {
 
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 	getQuote();
-	$('.random').click(function() {
+	$('.random').click(function () {
 		getQuote();
 	})
 });
